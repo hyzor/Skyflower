fee836e34aa1f8b2b60ec4745d630157393b88e5
Grottkjell <grottkjell@gmail.com>
2013-11-28 14:38:13 +0100

    Merge branch 'FysikMotor'
    Conflicts:
    	src/skyflower/main.cpp

---
+++ src/physics/Physics.cpp
@@ -0,0 +1,87 @@
+#include "Physics.h"
+
+Physics::Physics()
+{
+	this->start = std::clock();
+
+	this->gravity = DEFAULT_GRAVITY;
+	this->mass = DEFAULT_MASS;
+	/*this->gDeltaTime = 0.0f;
+	this->jDeltaTime = 0.0f;
+	this->pDeltaTime = 0.0f;*/
+	this->projectileAngle = 0.0f;
+	this->dt = 0.0;
+}
+
+
+Physics::Physics(const Physics& other)
+{
+	this->start = std::clock();
+
+	this->gravity = other.gravity;
+	this->mass = other.mass;
+	/*this->gDeltaTime = other.gDeltaTime;
+	this->jDeltaTime = other.jDeltaTime;
+	this->pDeltaTime = other.pDeltaTime;*/
+	this->projectileAngle = other.projectileAngle;
+	this->dt = 0.0;
+}
+
+
+Physics::~Physics(){}
+
+void Physics::setGravity(float x, float y, float z)
+{
+	this->gravity.X = x;
+	this->gravity.Y = y;
+	this->gravity.Z = z;
+}
+
+void Physics::setMass(float m)
+{
+	this->mass = m;
+}
+
+float Physics::getMass() const
+{
+	return this->mass;
+}
+
+Vec3 Physics::getGravity() const
+{
+	return this->gravity;
+}
+
+void Physics::update(float dt)
+{
+	this->dt = dt;
+}
+
+void Physics::addGravityCalc(Vec3 &pos, Vec3 &velocity, Vec3 &acceleration)
+{
+	Vec3 previousVelocity = velocity;
+	Vec3 previousPos = pos;
+	Vec3 previousAcc = acceleration;
+
+	velocity = previousVelocity + this->gravity * this->dt;
+	pos = previousPos + previousVelocity * this->dt + this->gravity * (this->dt * this->dt) / 2.0f;
+}
+
+void Physics::addGravityCalc(Vec3 &pos, Vec3 &velocity)
+{
+	Vec3 previousVelocity = velocity;
+	Vec3 previousPos = pos;
+
+	velocity = previousVelocity + this->gravity * this->dt;
+	pos = previousPos + previousVelocity * this->dt + this->gravity * (this->dt * this->dt) / 2.0f;
+}
+
+void Physics::addGravityCalc(Vec3 &pos)
+{
+	Vec3 previousPos = pos;
+	Vec3 previousVelocity = DEFAULT_VELOCITY_FALL;
+
+	previousVelocity = previousVelocity + this->gravity * this->dt;
+	pos = previousPos + previousVelocity * this->dt + this->gravity * (this->dt * this->dt) / 2.0f;
+}
+
--- src/skyflower/main.cpp
+++ src/skyflower/main.cpp
@@ -1,8 +1,11 @@
 #include <cstdio>
 #include <iostream>
+#include <sstream>
+#include <ctime>
 
 #include "shared/platform.h"
 #include "physics/Collision.h"
+#include "physics/Physics.h"
 #include "Application.h"
 
 //int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
@@ -12,9 +15,10 @@ int main(int argc, const char *argv[])
 
 	std::cout << "Debug!" << std::endl;
 
-	Box b1 = Box(10,0,0,10,10,10);
+
+	Box b1 = Box(10, 0, 0, 10, 10, 10);
 	Box b2 = Box(19, 9, -9, 10, 10, 10);
-	if(b1.Test(b2))
+	if (b1.Test(b2))
 		std::cout << "box collision!" << std::endl;
 
 	Sphere s1 = Sphere(10, 0, 0, 5);
@@ -23,14 +27,97 @@ int main(int argc, const char *argv[])
 		std::cout << "sphere collision!" << std::endl;
 
 	Triangle t = Triangle(Vec3(), Vec3(1, 0), Vec3(0, 1));
-	Ray r = Ray(Vec3(0.1f,0.1f,-0.5f), Vec3(0,0,1));
+	Ray r = Ray(Vec3(0.1f, 0.1f, -0.5f), Vec3(0, 0, 1));
 	if (t.Test(r) > 0)
 		std::cout << "triangle collision! " << t.Test(r) << std::endl;
-	
+
 
 	CollisionInstance ci = Collision::GetInstance()->CreateCollisionInstance("TestBlock.obj", Vec3());
-	if(ci.Test(r) > 0)
+	if (ci.Test(r) > 0)
 		std::cout << "Model collision! " << ci.Test(r) << std::endl;
+
+	Physics p;
+	float dt; //represents delta time for the gameloop
+
+	Vec3 pos(0.0f, 0.0f, 0.0f); //represents the position of an entity in the world
+	Vec3 velo(0.0f, 0.0f, 0.0f); //same, but velocity
+
+	std::ostringstream ss;
+	std::string x, y, z;
+	std::string xVelo, yVelo, zVelo;
+	std::string iter;
+
+	ss << pos.X;
+	x = ss.str();
+
+	ss.str("");
+	ss << pos.Y;
+	y = ss.str();
+
+	ss.str("");
+	ss << pos.Z;
+	z = ss.str();
+
+	ss.str("");
+	ss << velo.X;
+	xVelo = ss.str();
+
+	ss.str("");
+	ss << velo.Y;
+	yVelo = ss.str();
+
+	ss.str("");
+	ss << velo.Z;
+	zVelo = ss.str();
+
+	std::cout << "Position_0 - X: " + x + " Y: " + y + " Z: " + z + " \n";
+	std::cout << "Velocity_0 - X: " + xVelo + " Y: " + yVelo + " Z: " + zVelo + " \n";
+
+	dt = 0.1f;
+	p.update(dt);
+
+	for (int i = 0; i < 100; i++)
+	{
+		//TO be done: do collisioncheck here and decide whether or not to call Physics::addGravityCalc(), or do collisioncheck inside physics class
+		p.update(dt); //unnecessary in this example, but should be called before any calculations
+		p.addGravityCalc(pos, velo);
+
+		ss.str("");
+		ss << pos.X;
+		x = ss.str();
+
+		ss.str("");
+		ss << pos.Y;
+		y = ss.str();
+
+		ss.str("");
+		ss << pos.Z;
+		z = ss.str();
+
+		ss.str("");
+		ss << velo.X;
+		xVelo = ss.str();
+
+		ss.str("");
+		ss << velo.Y;
+		yVelo = ss.str();
+
+		ss.str("");
+		ss << velo.Z;
+		zVelo = ss.str();
+
+		ss.str("");
+		ss << (i + 1);
+		iter = ss.str();
+
+
+		std::cout << "Position_" + iter + " - X: " + x + " Y: " + y + " Z: " + z + " \n";
+		std::cout << "Velocity_" + iter + " - X: " + x + " Y: " + y + " Z: " + z + " \n";
+	}
+
+
+
+
 
 	app.Start();
 
