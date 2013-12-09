7009224552c0309e24361be2a78c009a58fd369e
Ulf Nilsson <kaksmet@gmail.com>
2013-12-09 18:33:04 +0100

    Restore changed lost in the graphics engine merge

--- src/skyflower/Application.cpp
+++ src/skyflower/Application.cpp
@@ -1,9 +1,23 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+
 #include "shared/util.h"
+#include "Sound/SoundEngine.h"
+
 #include "Application.h"
+#include "Cistron.h"
+#include "tinyxml2.h"
+#include "ComponentHeaders.h"
+
+using namespace std;
+using namespace tinyxml2;
+using namespace Cistron;
 
 Application::Application()
 {
 	m_window = NULL;
+	m_soundEngine = NULL;
 }
 
 Application::~Application()
@@ -15,11 +29,18 @@ void Application::Start()
 	m_window = new Window(1024, 768, L"Skyflower");
 	m_window->SetListener(this);
 
+	m_inputHandler = m_window->GetInputHandler();
+	m_inputHandler->SetListener(this);
+
 	// Create graphics engine
-	gEngine = CreateGraphicsEngine();
-	gEngine->Init(m_window->GetHandle());
-	m_quit = false;
-	CameraController *camera = gEngine->CreateCameraController();
+	m_graphicsEngine = CreateGraphicsEngine();
+	m_graphicsEngine->Init(m_window->GetHandle());
+
+	//m_soundEngine = CreateSoundEngine("../../content/sounds/");
+	//assert(m_soundEngine);
+
+	EntityManager *entityManager = new EntityManager();
+
 	//loading xml-file, creating entities and components to this entityManager
 	//entityManager->loadXML(entityManager, "test2.xml");
 
@@ -30,39 +51,53 @@ void Application::Start()
 	//entityManager->sendMessageToEntity("Hello", "Player");
 
 
-	gEngine->CreateInstance("Data\\Models\\duck.obj")->SetVisibility(false);
 
-	ModelInstance* d = gEngine->CreateInstance("Data\\Models\\duck.obj", Vec3(-100, 50, 0));
+	CameraController *camera = m_graphicsEngine->CreateCameraController();
+	
+	m_graphicsEngine->CreateInstance("Data\\Models\\duck.obj")->SetVisibility(false);
+
+	ModelInstance* d = m_graphicsEngine->CreateInstance("Data\\Models\\duck.obj", Vec3(-100, 50, 0));
 	d->SetRotation(Vec3(-3.14f/2, 3.14f/4));
 
+
+
 	float oldTime = GetTime();
-	float time, timeElapsed;
+	float time, deltaTime;
+
+	m_quit = false;
 
 	while(!m_quit)
 	{
 		time = GetTime();
-		timeElapsed = time - oldTime;
+		deltaTime = time - oldTime;
 		oldTime = time;
+
+
 
 		d->SetPosition(d->GetPosition() + Vec3(0.01f, 0.0f, 0.0f));
 		Vec3 dir = (d->GetPosition() - camera->GetPosition()).Normalize();
 
 		camera->SetDirection(dir);
 		camera->SetPosition(d->GetPosition() - dir * 1000);
-		gEngine->DrawScene();
-		gEngine->UpdateScene(timeElapsed);
+
+
+
+		m_graphicsEngine->DrawScene();
+		m_graphicsEngine->UpdateScene(deltaTime);
+
+		//m_soundEngine->Update(deltaTime);
 
 		m_window->PumpMessages();
 	}
 
-	gEngine->DeleteInstance(d);
+	m_graphicsEngine->DeleteInstance(d);
 
+
+
+	delete entityManager;
 	//DestroySoundEngine(m_soundEngine);
-
-	//delete entityManager;
-
+	DestroyGraphicsEngine(m_graphicsEngine);
 	delete m_window;
-	DestroyGraphicsEngine(gEngine);
 }
 
 void Application::OnWindowShouldClose()
@@ -71,5 +106,29 @@ void Application::OnWindowShouldClose()
 }
 
 void Application::OnWindowResize(unsigned int width, unsigned int height)
+{
+}
+
+void Application::OnMouseMove(int deltaX, int deltaY)
+{
+}
+
+void Application::OnMouseButtonDown(enum MouseButton button)
+{
+}
+
+void Application::OnMouseButtonUp(enum MouseButton button)
+{
+}
+
+void Application::OnMouseWheel(int delta)
+{
+}
+
+void Application::OnKeyDown(unsigned short key)
+{
+}
+
+void Application::OnKeyUp(unsigned short key)
 {
 }
