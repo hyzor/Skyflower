45f664f176e23920148f407f554bef0b5b3c413e
Ulf Nilsson <kaksmet@gmail.com>
2013-12-10 14:10:54 +0100

    Recover changes lost in merge

--- src/skyflower/Application.cpp
+++ src/skyflower/Application.cpp
@@ -1,11 +1,23 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+
 #include "shared/util.h"
-#include "EntityManager.h"
+#include "Sound/SoundEngine.h"
+
 #include "Application.h"
-#include "ScriptHandler.h"
+#include "tinyxml2.h"
+#include "ComponentHeaders.h"
+
+using namespace std;
+using namespace tinyxml2;
+using namespace Cistron;
 
 Application::Application()
 {
 	m_window = NULL;
+	m_soundEngine = NULL;
+	this->entityManager = NULL;
 }
 
 Application::~Application()
@@ -17,63 +29,75 @@ void Application::Start()
 	m_window = new Window(1024, 768, L"Skyflower");
 	m_window->SetListener(this);
 
-	// Create graphics engine
-	gEngine = CreateGraphicsEngine();
-	gEngine->Init(m_window->GetHandle());
+	m_inputHandler = m_window->GetInputHandler();
+	m_inputHandler->SetListener(this);
 
-	m_quit = false;
+	// Create graphics engine
+	m_graphicsEngine = CreateGraphicsEngine();
+	m_graphicsEngine->Init(m_window->GetHandle(), m_window->GetWidth(), m_window->GetHeight());
+
+	//m_soundEngine = CreateSoundEngine("../../content/sounds/");
+	//assert(m_soundEngine);
+
+	entityManager = new EntityManager(m_graphicsEngine);
 
 	//loading xml-file, creating entities and components to this entityManager
-	EntityManager * entityManager = new EntityManager();
-	entityManager->loadXML(entityManager, "test3.xml");
-	
+	entityManager->loadXML(entityManager, "test2.xml");
 
 	////sends a message to all components in all entities in that manager
-	entityManager->sendMessageToAllEntities("Hello");
+	//entityManager->sendMessageToAllEntities("Hello");
 
 	////sends a message to a specific entity, in this case a Player-entity.
 	//entityManager->sendMessageToEntity("Hello", "Player");
 
-	//ScriptHandler *script = ScriptHandler::GetInstance();
-	//script->Run("test.lua");
 
-	gEngine->CreateInstance("Data\\Models\\duck.obj")->SetVisibility(false);
 
-	ModelInstance* d = gEngine->CreateInstance("Data\\Models\\duck.obj", Vec3(-100, 50, 0));
-	d->SetRotation(Vec3(-3.14f/2, 3.14f/4));
+	CameraController *camera = m_graphicsEngine->CreateCameraController();
+	
+	//m_graphicsEngine->CreateInstance("Data\\Models\\duck.obj")->SetVisibility(false);
+
+	//ModelInstance* d = m_graphicsEngine->CreateInstance("Data\\Models\\duck.obj", Vec3(-100, 50, 0));
+	//d->SetRotation(Vec3(-3.14f/2, 3.14f/4));
+
+
 
 	float oldTime = GetTime();
-	float time, timeElapsed;
+	float time, deltaTime;
 
-	//if (script->getMessage())
-	//{
-	//	entityManager->sendMessageToAllEntities("Hello");
-	//	entityManager->sendMessageToEntity("Hello", "Player2");
-	//}
+	m_quit = false;
 
 	while(!m_quit)
 	{
 		time = GetTime();
-		timeElapsed = time - oldTime;
+		deltaTime = time - oldTime;
 		oldTime = time;
 
-		d->SetPosition(d->GetPosition() + Vec3(0.01f, 0.0f, 0.0f));
 
 
-		gEngine->DrawScene();
-		gEngine->UpdateScene(timeElapsed);
+		//d->SetPosition(d->GetPosition() + Vec3(0.01f, 0.0f, 0.0f));
+		//Vec3 dir = (d->GetPosition() - camera->GetPosition()).Normalize();
+
+		//camera->SetDirection(dir);
+		//camera->SetPosition(d->GetPosition() - dir * 1000);
+
+
+
+		m_graphicsEngine->DrawScene();
+		m_graphicsEngine->UpdateScene(deltaTime);
+
+		//m_soundEngine->Update(deltaTime);
 
 		m_window->PumpMessages();
 	}
 
-	gEngine->DeleteInstance(d);
+	//m_graphicsEngine->DeleteInstance(d);
 
+
+
+	delete entityManager;
 	//DestroySoundEngine(m_soundEngine);
-
-	//delete entityManager;
-
+	DestroyGraphicsEngine(m_graphicsEngine);
 	delete m_window;
-	DestroyGraphicsEngine(gEngine);
 }
 
 void Application::OnWindowShouldClose()
@@ -82,5 +106,47 @@ void Application::OnWindowShouldClose()
 }
 
 void Application::OnWindowResize(unsigned int width, unsigned int height)
+{
+}
+
+void Application::OnMouseMove(int deltaX, int deltaY)
+{
+}
+
+void Application::OnMouseButtonDown(enum MouseButton button)
+{
+}
+
+void Application::OnMouseButtonUp(enum MouseButton button)
+{
+}
+
+void Application::OnMouseWheel(int delta)
+{
+}
+
+void Application::OnKeyDown(unsigned short key)
+{
+	switch (key)
+	{
+	case 'W':
+		entityManager->sendMessageToEntity("W", "Player");
+		break;
+	case 'S':
+		entityManager->sendMessageToEntity("S", "Player");
+		break;
+	case 'A':
+		entityManager->sendMessageToEntity("A", "Player");
+		break;
+	case 'D':
+		entityManager->sendMessageToEntity("D", "Player");
+		break;
+	case 'E':
+		entityManager->sendMessageToEntity("E", "Player");
+		break;
+	}
+}
+
+void Application::OnKeyUp(unsigned short key)
 {
 }
