41e8cf7d8148f84591eec9728d22509d72a95cd8
Ulf Nilsson <kaksmet@gmail.com>
2013-12-17 09:52:40 +0100

    SoundEngine: various opus decoding fixes

--- src/sound/AudioDecoderOpus.cpp
+++ src/sound/AudioDecoderOpus.cpp
@@ -8,6 +8,16 @@
 #include "AudioDecoderOpus.h"
 #include "AudioResource.h"
 #include "Config.h"
+#include "TaskQueueWin32.h"
+
+struct OpusDecoderContext
+{
+	OggOpusFile *opus;
+	Mutex *mutex;
+	// In number of samples.
+	size_t bufferSize;
+	int16_t *buffer;
+};
 
 bool AudioDecoderOpusInit(struct AudioResource *resource)
 {
@@ -17,10 +27,10 @@ bool AudioDecoderOpusInit(struct AudioResource *resource)
 		return false;
 	}
 
-	int64_t totalSamples = op_pcm_total(opus, -1);
 	int channels = op_channel_count(opus, -1);
+	int64_t totalSamples = op_pcm_total(opus, -1) * channels;
 
-	if (op_seekable(opus) == 0 || totalSamples < 0 || channels > 2) {
+	if (op_seekable(opus) == 0 || channels > 2 || totalSamples < 0) {
 		op_free(opus);
 		return false;
 	}
@@ -28,17 +38,31 @@ bool AudioDecoderOpusInit(struct AudioResource *resource)
 	// Opus is fixed at 16-bit and 48kHz sample rate.
 	unsigned int bitDepth = 16;
 	unsigned int sampleRate = 48000;
-	// FIXME: Fix and enable streaming.
-	//bool shouldStream = (resource->file->size > SOUNDENGINE_STREAM_THRESHOLD_SIZE? true : false);
+
+	// FIXME: Fix the clicking noise when streaming and enable it again.
+	//bool shouldStream = ((totalSamples * (bitDepth / 8)) > SOUNDENGINE_STREAM_THRESHOLD_SIZE? true : false);
 	bool shouldStream = false;
+
+	uint64_t samplesPerBuffer = (shouldStream? std::min((int64_t)(SOUNDENGINE_STREAM_BUFFER_SIZE * sampleRate * channels), totalSamples) : totalSamples);
+
+	struct OpusDecoderContext *context = new struct OpusDecoderContext;
+	context->opus = opus;
+	context->mutex = new MutexWin32();
+	context->bufferSize = 0;
+	context->buffer = NULL;
+
+	if (shouldStream) {
+		context->bufferSize = (size_t)samplesPerBuffer;
+		context->buffer = new int16_t[context->bufferSize];
+	}
 
 	resource->info.format = (channels == 1? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16);
 	resource->info.totalSamples = totalSamples;
-	resource->info.samplesPerBuffer = (shouldStream? std::min((int64_t)(SOUNDENGINE_STREAM_BUFFER_SIZE * sampleRate * channels), totalSamples) : totalSamples);
+	resource->info.samplesPerBuffer = samplesPerBuffer;
 	resource->info.channels = channels;
 	resource->info.sampleRate = sampleRate;
 	resource->info.bitDepth = bitDepth;
-	resource->context = (void *)opus;
+	resource->context = (void *)context;
 
 	//printf("AudioDecoderOpus, shouldStream=%d, bufferCount=%d, channels=%d, sampleRate=%d, bitDepth=%d\n", shouldStream, (int)ceil(resource->info.totalSamples / (float)resource->info.samplesPerBuffer), channels, sampleRate, bitDepth);
 
@@ -51,8 +75,16 @@ void AudioDecoderOpusRelease(struct AudioResource *resource)
 		return;
 	}
 
-	OggOpusFile *opus = (OggOpusFile *)resource->context;
-	op_free(opus);
+	struct OpusDecoderContext *context = (struct OpusDecoderContext *)resource->context;
+
+	if (context->buffer) {
+		delete[] context->buffer;
+	}
+
+	delete context->mutex;
+	op_free(context->opus);
+
+	delete context;
 }
 
 void AudioDecoderOpusFillBuffer(const struct AudioResource *resource, uint64_t sampleOffset, uint64_t sampleCount, ALuint buffer)
@@ -61,23 +93,31 @@ void AudioDecoderOpusFillBuffer(const struct AudioResource *resource, uint64_t s
 	assert(sampleOffset + sampleCount <= resource->info.totalSamples);
 	assert(alIsBuffer(buffer));
 
-	OggOpusFile *opus = (OggOpusFile *)resource->context;
-	int64_t position = op_pcm_tell(opus);
+	struct OpusDecoderContext *context = (struct OpusDecoderContext *)resource->context;
+	context->mutex->Lock();
+
+	int64_t position = op_pcm_tell(context->opus) * resource->info.channels;
 
 	assert(position >= 0);
 
 	if (position != sampleOffset) {
-		assert(op_pcm_seek(opus, sampleOffset) == 0);
+		assert(op_pcm_seek(context->opus, sampleOffset) == 0);
 	}
 
-	// FIXME: Don't alloc this every time.
-	// We can't have a single reusable buffer per file since this can be called from multiple threads and
-	// if they all share a single buffer they would overwrite each other's samples.
-	int16_t *data = new int16_t[(size_t)sampleCount];
 	int samplesRead = 0;
-	
+	int16_t *data;
+
+	if (context->buffer) {
+		assert(context->bufferSize >= sampleCount);
+
+		data = context->buffer;
+	}
+	else {
+		data = new int16_t[(size_t)sampleCount];
+	}
+
 	while (samplesRead < sampleCount) {
-		int result = op_read(opus, data + samplesRead, (int)sampleCount - samplesRead, NULL);
+		int result = op_read(context->opus, data + samplesRead, (int)sampleCount - samplesRead, NULL);
 
 		assert(result > 0);
 
@@ -85,8 +125,15 @@ void AudioDecoderOpusFillBuffer(const struct AudioResource *resource, uint64_t s
 		samplesRead += result * resource->info.channels;
 	}
 
-	alBufferData(buffer, resource->info.format, (const void *)data, (ALsizei)(sampleCount * sizeof(uint16_t)), resource->info.sampleRate);
-	delete[] data;
+	assert(samplesRead <= sampleCount);
+
+	alBufferData(buffer, resource->info.format, (const void *)data, (ALsizei)(samplesRead * sizeof(uint16_t)), resource->info.sampleRate);
+
+	if (!context->buffer) {
+		delete[] data;
+	}
+
+	context->mutex->Unlock();
 
 	assert(alGetError() == AL_NO_ERROR);
 }
--- src/sound/Config.h
+++ src/sound/Config.h
@@ -9,7 +9,7 @@
 // Twice as many since streaming sources will use double buffering.
 #define SOUNDENGINE_BUFFER_POOL_SIZE       (SOUNDENGINE_SOURCE_POOL_SIZE * 2)
 
-// Sound files larger than this will be streamed.
+// Sound files larger than this in bytes when decoded will be streamed.
 #define SOUNDENGINE_STREAM_THRESHOLD_SIZE  (1 * 1024 * 1024)
 // How many samples, in seconds, each buffer will hold for streaming resources.
 #define SOUNDENGINE_STREAM_BUFFER_SIZE     0.25f
