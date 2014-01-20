9fdc7b800f9ddd61caf332d97718a9a6ab9dd0b8
Ulf Nilsson <kaksmet@gmail.com>
2014-01-20 08:59:05 +0100

    Ta bört lite död kod

--- src/skyflower/EntityManager.h
+++ src/skyflower/EntityManager.h
@@ -127,7 +127,6 @@ class EntityManager {
 		void sendMessageToAllEntities(string message); //sends a message to all components in all entities in that manager
 		void sendMessageToEntity(string message, string entity); //sends a message to a specific entity
 		void sendMessageToEntity(string message, EntityId entity); //sends a message to a specific entity
-		bool loadXML(EntityManager * entityManager, string xmlFile);
 		bool loadXML2(string xmlFile);
 
 		Vec3 getEntityPos(string type);
