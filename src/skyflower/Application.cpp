1bfe0360afcec952dcaddda30cd34c4555949acf
Yokali <Lindis_Yokali@hotmail.com>
2014-03-04 11:39:57 +0100

    Level 1 skylten "fixad"

--- src/skyflower/Application.cpp
+++ src/skyflower/Application.cpp
@@ -107,7 +107,7 @@ void Application::Start()
 	levelHandler->init(m_entityManager);
 
 	// Load Hub Level
-	levelHandler->queue(5);
+	levelHandler->queue(0);
 	//levelHandler->loadQueued();
 	levelHandler->LoadQueued(mXmlResourceDir);
 	//levelHandler->loadQueued();
