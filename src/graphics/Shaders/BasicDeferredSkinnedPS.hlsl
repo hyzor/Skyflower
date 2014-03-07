c1c46fb31e201715b18a33880806e11627014686
Grottkjell <grottkjell@gmail.com>
2014-03-07 15:08:49 +0100

    Same for morphed/skinned.

--- src/graphics/Shaders/BasicDeferredSkinnedPS.hlsl
+++ src/graphics/Shaders/BasicDeferredSkinnedPS.hlsl
@@ -44,7 +44,8 @@ PixelOut main(VertexOut pIn)
 	//pOut.Position = float4(pIn.PosW, 1.0f);
 
 	// Bake shadow factor into color w component
-	float shadowFactor = 1.0f;//CalcShadowFactor(samShadow, gShadowMap, pIn.ShadowPosH);
+	float shadowFactor = 1.0f;
+	float shadowFactorBlend = 1.0f;
 
 	int cascadeIndex = 0;
 	int nrOfCascades = gNrOfCascades;
@@ -53,26 +54,38 @@ PixelOut main(VertexOut pIn)
 	float4 shadowPosH2 = pIn.ShadowPosH2;
 	float4 shadowPosH3 = pIn.ShadowPosH3;
 	float4 shadowPosH4 = pIn.ShadowPosH4;
+	int blendIndex = -1;
 
 	//Compare the depth of current pixel in camera space to given near and far depths
 	//to decide appropriate index of cascade to sample from
 	if (nrOfCascades == 1 || depth > gNearDepths.x && depth < gFarDepths.x)
 	{
 		cascadeIndex = 0;
+		if ((gFarDepths.x - depth) < BLEND_LIMIT_1)
+			blendIndex = 1;
 	}
-	if (nrOfCascades > 1 && depth > gNearDepths.y && depth < gFarDepths.y)
+	if (nrOfCascades > 1 && (depth > gNearDepths.y && depth < gFarDepths.y))
 	{
 		cascadeIndex = 1;
+		if ((depth - gNearDepths.x) < BLEND_LIMIT_1)
+			blendIndex = 0;
+		else if ((gFarDepths.y - depth) < BLEND_LIMIT_2)
+			blendIndex = 2;
 	}
-	if (nrOfCascades > 2 && depth > gNearDepths.z && depth < gFarDepths.z)
+	if (nrOfCascades > 2 && (depth > gNearDepths.z && depth < gFarDepths.z))
 	{
 		cascadeIndex = 2;
+		if ((depth - gNearDepths.y) < BLEND_LIMIT_2)
+			blendIndex = 1;
+		else if ((gFarDepths.z - depth) < BLEND_LIMIT_3)
+			blendIndex = 3;
 	}
-	if (nrOfCascades > 3 && depth > gNearDepths.w && depth < gFarDepths.w)
+	if (nrOfCascades > 3 && (depth > gNearDepths.w && depth < gFarDepths.w))
 	{
 		cascadeIndex = 3;
+		if ((depth - gNearDepths.z) < BLEND_LIMIT_3)
+			blendIndex = 2;
 	}
-
 	if (cascadeIndex == 0)
 	{
 		shadowFactor = CalcShadowFactor(samShadow, gShadowMap1, shadowPosH1); //Cascade 1
@@ -88,6 +101,27 @@ PixelOut main(VertexOut pIn)
 	else if (cascadeIndex == 3)
 	{
 		shadowFactor = CalcShadowFactor(samShadow, gShadowMap4, shadowPosH4); //Cascade 3
+	}
+
+	switch (blendIndex)
+	{
+	case 0:
+		shadowFactorBlend = CalcShadowFactor(samShadow, gShadowMap1, shadowPosH1);
+		break;
+	case 1:
+		shadowFactorBlend = CalcShadowFactor(samShadow, gShadowMap2, shadowPosH2);
+		break;
+	case 2:
+		shadowFactorBlend = CalcShadowFactor(samShadow, gShadowMap3, shadowPosH3);
+		break;
+	case 3:
+		shadowFactorBlend = CalcShadowFactor(samShadow, gShadowMap4, shadowPosH4);
+		break;
+	}
+
+	if (blendIndex != -1)
+	{
+		shadowFactor = lerp(shadowFactor, shadowFactorBlend, 0.5f);
 	}
 
 	pOut.Normal.w = shadowFactor;
